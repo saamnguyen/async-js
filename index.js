a428b15c265f6f452ac495ca8ceee6ea89fa4274
finn-01 <nguyensam576@gmail.com>
2022-03-30 16:41:49 +0700

    callback hell

--- index.js
+++ index.js
@@ -17,4 +17,13 @@ function httpGetAsync(theUrl, callback) {
 
 httpGetAsync("https://picsum.photos/200/300", (data) => {
 	console.log(data);
+	document.getElementById("img_1").setAttribute("src", data.responseURL);
+
+	httpGetAsync("https://picsum.photos/200/300", (data) => {
+		document.getElementById("img_2").setAttribute("src", data.responseURL);
+
+		httpGetAsync("https://picsum.photos/200/300", (data) => {
+			document.getElementById("img_3").setAttribute("src", data.responseURL);
+		});
+	});
 });
