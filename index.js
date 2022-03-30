ad5232063d5def1a0e03db959a27e1db6788fa6c
finn-01 <nguyensam576@gmail.com>
2022-03-30 16:26:08 +0700

    callback

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 function test() {
-	console.log(1);
+	setTimeout(() => console.log(1), 0);
 	console.log(2);
 	console.log(3);
 }
 
 test();
+
+function httpGetAsync(theUrl, callback) {
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.onreadystatechange = function () {
+		if (xmlHttp.readyState == 4 && xmlHttp.status == 200) callback(xmlHttp);
+	};
+	xmlHttp.open("GET", theUrl, true);
+	xmlHttp.send(null);
+}
+
+httpGetAsync("https://picsum.photos/200/300", (data) => {
+	console.log(data);
+});
