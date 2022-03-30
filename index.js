6ffee82369cb91f5df9887b06557f986bbcafbb6
finn-01 <nguyensam576@gmail.com>
2022-03-30 17:01:11 +0700

    promises

--- index.js
+++ index.js
@@ -1,29 +1,102 @@
-function test() {
-	setTimeout(() => console.log(1), 0);
-	console.log(2);
-	console.log(3);
-}
+// function test() {
+// 	setTimeout(() => console.log(1), 0);
+// 	console.log(2);
+// 	console.log(3);
+// }
 
-test();
+// test();
 
-function httpGetAsync(theUrl, callback) {
+// function httpGetAsync(theUrl, callback) {
+// 	var xmlHttp = new XMLHttpRequest();
+// 	xmlHttp.onreadystatechange = function () {
+// 		if (xmlHttp.readyState == 4 && xmlHttp.status == 200) callback(xmlHttp);
+// 	};
+// 	xmlHttp.open("GET", theUrl, true);
+// 	xmlHttp.send(null);
+// }
+
+// httpGetAsync("https://picsum.photos/200/300", (data) => {
+// 	console.log(data);
+// 	document.getElementById("img_1").setAttribute("src", data.responseURL);
+
+// 	httpGetAsync("https://picsum.photos/200/300", (data) => {
+// 		document.getElementById("img_2").setAttribute("src", data.responseURL);
+
+// 		httpGetAsync("https://picsum.photos/200/300", (data) => {
+// 			document.getElementById("img_3").setAttribute("src", data.responseURL);
+// 		});
+// 	});
+// });
+
+//Promises
+
+function httpGetAsync1(theUrl, resolve) {
 	var xmlHttp = new XMLHttpRequest();
 	xmlHttp.onreadystatechange = function () {
-		if (xmlHttp.readyState == 4 && xmlHttp.status == 200) callback(xmlHttp);
+		if (xmlHttp.readyState == 4 && xmlHttp.status == 200) resolve(xmlHttp);
 	};
 	xmlHttp.open("GET", theUrl, true);
 	xmlHttp.send(null);
 }
 
-httpGetAsync("https://picsum.photos/200/300", (data) => {
-	console.log(data);
-	document.getElementById("img_1").setAttribute("src", data.responseURL);
+const currentPromise = new Promise((resolve, reject) => {
+	// let condition = true;
 
-	httpGetAsync("https://picsum.photos/200/300", (data) => {
+	// if (condition) {
+	// 	setTimeout(() => {
+	// 		resolve("Success");
+	// 	}, 3000);
+	// } else {
+	// 	reject("Error");
+	// }
+
+	httpGetAsync1("https://picsum.photos/200/300", resolve);
+});
+
+const currentPromise1 = new Promise((resolve, reject) => {
+	// let condition = true;
+
+	// if (condition) {
+	// 	setTimeout(() => {
+	// 		resolve("Success");
+	// 	}, 3000);
+	// } else {
+	// 	reject("Error");
+	// }
+
+	httpGetAsync1("https://picsum.photos/200/300", resolve);
+});
+
+const currentPromise2 = new Promise((resolve, reject) => {
+	// let condition = true;
+
+	// if (condition) {
+	// 	setTimeout(() => {
+	// 		resolve("Success");
+	// 	}, 3000);
+	// } else {
+	// 	reject("Error");
+	// }
+
+	httpGetAsync1("https://picsum.photos/200/300", resolve);
+});
+
+//Khi thanh cong thi then se dc goi
+currentPromise
+	.then((data) => {
+		console.log(data);
+		document.getElementById("img_1").setAttribute("src", data.responseURL);
+
+		return currentPromise1;
+	})
+	.then((data) => {
 		document.getElementById("img_2").setAttribute("src", data.responseURL);
 
-		httpGetAsync("https://picsum.photos/200/300", (data) => {
-			document.getElementById("img_3").setAttribute("src", data.responseURL);
-		});
+		return currentPromise2;
+	})
+	.then((data) => {
+		document.getElementById("img_3").setAttribute("src", data.responseURL);
+	})
+	.catch((err) => {
+		console.log(err);
 	});
-});
